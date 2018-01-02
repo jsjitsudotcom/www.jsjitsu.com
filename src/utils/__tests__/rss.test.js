4737c7d4d1d660261ef68fc570c68bf0af8d42ee
Marques Kevin <marques.developpement@gmail.com>
2018-01-02 22:25:01 +0100

    add rss

--- src/utils/__tests__/rss.test.js
+++ src/utils/__tests__/rss.test.js
@@ -1,10 +1,34 @@
-import { fetchSource } from "./../rss";
+import Rss from "./../rss";
+import sinon from "sinon";
+import feedsData from "./__data__/feed.json";
+
+Rss.fetchFeeds = () => {
+  throw new Error("fetchFeeds ne peut pas etre utilise dans un test");
+};
 
 describe("rss tests suite", () => {
   it("should return feeds from source", () => {
-    return fetchSource("Hacker News", "https://news.ycombinator.com/rss").then((source) => {
+    const stubFetchFeeds = sinon.stub(Rss, "fetchFeeds").resolves(feedsData);
+    return Rss.fetchSource(
+      "Hacker News",
+      "https://news.ycombinator.com/rss"
+    ).then(source => {
       expect(source.name).toEqual("Hacker News");
       expect(source.feeds).toHaveLength(30);
-    })
+      expect(source.description).toBeTruthy();
+      expect(source.link).toBeTruthy();
+      expect(source.title).toBeTruthy();
+      stubFetchFeeds.restore();
+    });
+  });
+
+  it("should catch an error", () => {
+    const stubFetchFeeds = sinon
+      .stub(Rss, "fetchFeeds")
+      .rejects({ message: "Error" });
+    return Rss.fetchSource().catch(error => {
+      expect(error.message).toEqual("Error");
+      stubFetchFeeds.restore();
+    });
   });
 });
--- src/utils/rss.js
+++ src/utils/rss.js
@@ -1,15 +1,26 @@
 import rssParser from "rss-parser";
 import Promise from "bluebird";
 
-export const fetchFeeds = url => {
-  return new Promise((resolve, reject) => {
-    return rssParser.parseURL(url, (err, parsed) => {
-      if (err) return reject(err);
-      return resolve(parsed);
+const Rss = {
+  fetchFeeds: /* istanbul ignore next */ (url) => {  
+    return new Promise((resolve, reject) => {
+      return rssParser.parseURL(url, (err, parsed) => {
+        if (err) return reject(err);
+        return resolve(parsed);
+      });
     });
-  });
+  },
+  fetchSource(source, url) {
+    return Rss.fetchFeeds(url).then(rss => {
+      return {
+        feeds: rss.feed.entries,
+        name: source,
+        link: rss.feed.link,
+        description: rss.feed.description,
+        title: rss.feed.title
+      };
+    });
+  }
 };
 
-export const fetchSource = (source, url) => {
-  
-};
+export default Rss;
