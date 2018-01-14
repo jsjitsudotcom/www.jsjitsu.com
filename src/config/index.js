8950a4c3f66cc24377b0ac6edf6645fb9a55cad6
Marques Kevin <marques.developpement@gmail.com>
2018-01-14 21:59:59 +0100

    ajout des services workers cache et optimisation du build

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export const RSS_ENDPOINT = "https://jdys2ftxag.execute-api.eu-west-1.amazonaws.com/production";
-// export const RSS_ENDPOINT = "http://192.168.43.165:3001";
+// export const RSS_ENDPOINT = "http://localhost:7654";
--- src/utils/rss.js
+++ src/utils/rss.js
@@ -1,23 +1,17 @@
-import rssParser from "rss-parser";
-import Promise from "bluebird";
 import { RSS_ENDPOINT } from "./../config";
 
 const RSS = {
-  fetchFeeds: /* istanbul ignore next */ (url) => {  
-    return new Promise((resolve, reject) => {
-      return rssParser.parseURL(`${RSS_ENDPOINT}/feeds?url=${url}`, (err, parsed) => {
-        if (err) return reject(err);
-        return resolve(parsed);
-      });
-    });
+  fetchFeeds: /* istanbul ignore next */ url => {
+    const endpoint = `${RSS_ENDPOINT}/feeds?url=${url}`;
+    return fetch(endpoint).then(response => response.json());
   },
   fetchSource(url) {
     return RSS.fetchFeeds(url).then(rss => {
       return {
-        feeds: rss.feed.entries,
-        link: rss.feed.link,
-        description: rss.feed.description,
-        title: rss.feed.title
+        feeds: rss.feeds,
+        link: rss.link,
+        description: rss.description,
+        title: rss.title
       };
     });
   }
