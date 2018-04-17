9ba7e3850700c4ab40db4c0e56fa846499b4876d
Marques Kevin <administrateur@MacBook-Air-de-Marques.local>
2018-04-18 00:18:13 +0200

    refactor(player): controls avec les states et lancement de la video

--- src/components/Player/components/Video/Video.js
+++ src/components/Player/components/Video/Video.js
@@ -13,6 +13,18 @@ const addInactive = style => isActive =>
 const isNextInactive = addInactive(Style.next);
 const isPreviousInactive = addInactive(Style.previous);
 
+const durationToText = duration => {
+  const minutes = Math.floor(duration / 60);
+  const secondes = Math.floor(duration % 60);
+
+  const hasMinutes =
+    minutes >= 10 ? `${minutes}:` : minutes > 0 ? `0${minutes}:` : "00:";
+  const hasSeconds =
+    secondes >= 10 ? `${secondes}` : secondes > 0 ? `0${secondes}` : "00";
+
+  return `${hasMinutes}${hasSeconds}`;
+};
+
 class Video extends PureComponent {
   constructor(props) {
     super(props);
@@ -22,6 +34,13 @@ class Video extends PureComponent {
     this.play = this.play.bind(this);
   }
 
+  state = {
+    duration: "00:00",
+    current: "00:00",
+    percentage: 0,
+    showControls: true
+  };
+
   componentDidMount() {
     this.play();
     this.hideControls();
@@ -29,23 +48,34 @@ class Video extends PureComponent {
 
   play() {
     this.video.play();
+
+    this.video.ontimeupdate = () => {
+      this.setState({
+        current: durationToText(this.video.currentTime),
+        duration: durationToText(this.video.duration),
+        percentage: this.video.currentTime / this.video.duration * 100
+      });
+    };
+
     this.video.style.backgroundImage = null;
   }
 
-  pause() {
+  pause(e) {
+    e.stopPropagation();
     this.video.pause();
   }
 
   hideControls() {
-    this.controls.style.display = "none";
+    this.setState({ showControls: false });
   }
 
   showControls() {
-    this.controls.style.display = "flex";
+    this.setState({ showControls: true });
   }
 
   onVideoClick() {
-    this.showControls();
+    if (this.state.showControls) return this.hideControls();
+    return this.showControls();
   }
 
   render() {
@@ -62,31 +92,40 @@ class Video extends PureComponent {
           <source src={source} type="video/mp4" />
         </video>
 
-        <div className={Style.controls} ref={ref => (this.controls = ref)}>
-          <div className={isPreviousInactive(hasPrevious)}>
-            <img src={Previous} alt="previous" />
-          </div>
+        {this.state.showControls && (
+          <div
+            className={Style.controls}
+            ref={ref => (this.controls = ref)}
+            onClick={this.onVideoClick}
+          >
+            <div className={isPreviousInactive(hasPrevious)}>
+              <img src={Previous} alt="previous" />
+            </div>
 
-          <div className={Style.play} onClick={this.pause}>
-            <img src={Play} alt="play" />
-          </div>
+            <div className={Style.play} onClick={this.pause}>
+              <img src={Play} alt="play" />
+            </div>
 
-          <div className={isNextInactive(hasNext)}>
-            <img src={Next} alt="next" />
+            <div className={isNextInactive(hasNext)}>
+              <img src={Next} alt="next" />
+            </div>
           </div>
-        </div>
+        )}
 
         <div className={Style.bottom}>
           <div className={Style.infoProgress}>
-            <div className={Style.currentTime}>00:23</div>
-            <div className={Style.duration}>02:34</div>
+            <div className={Style.currentTime}>{this.state.current}</div>
+            <div className={Style.duration}>{this.state.duration}</div>
             <div className={Style.expand}>
               <FullScreen />
             </div>
           </div>
 
           <div className={Style.progress}>
-            <div className={Style.progressCurrent} />
+            <div
+              className={Style.progressCurrent}
+              style={{ width: `${this.state.percentage}%` }}
+            />
             <div className={Style.cursor} />
           </div>
         </div>
